9d9ce8505b43b72a15e398c51037a9ad48bfd4c8
SKYGITS-66 <renaldisigit69@gmail.com>
2026-08-11 17:21:45 +0000

    tes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import yt_dlp
+import re
 
 app = Flask(__name__)
 CORS(app)
@@ -11,15 +12,19 @@ def home():
 
 @app.route("/get-audio", methods=["GET"])
 def get_youtube_audio():
-    url = request.args.get("url")
-    if not url:
+    raw_url = request.args.get("url")
+    if not raw_url:
         return jsonify({"error": "Parameter 'url' diperlukan"}), 400
+    
+    # Bersihkan URL dari parameter list/radio yang mengganggu
+    url = raw_url.split("&")[0]
     
     try:
         ydl_opts = {
-            'format': 'best',  # Menggunakan format terbaik yang fleksibel agar tidak error
+            'format': 'bestaudio',
+            'noplaylist': True,  # Mencegah yt-dlp memuat playlist/radio
             'quiet': True,
-            'cookiefile': 'cookies.txt',  # Pastikan file cookies.txt ada di folder yang sama
+            'cookiefile': 'cookies.txt',  # Pastikan file cookies.txt ada
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
